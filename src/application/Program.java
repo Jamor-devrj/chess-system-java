44b2ce379caf9f7da86c2040d76f3f05ddec6b27
jamor <jamor.devbr@gmail.com>
2023-06-03 10:44:22 -0300

    Starting to implement Board and Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,13 @@
 package application;
 
-import boardgame.Position;
+import boardgame.Board; 
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Position pos = new Position(8, 3);
+		Board board = new Board(8, 8);		
 		
-		System.out.println(pos);		
-
 	}
 
 }
---
+++ src/boardgame/Board.java
@@ -0,0 +1,19 @@
+package boardgame;
+
+public class Board {
+	
+	private int rows;
+	private int columns;
+	private Piece[][] pieces; //Matriz de peças
+	
+	public Board(int rows, int columns) {		
+		this.rows = rows;
+		this.columns = columns;
+		pieces = new Piece[rows][columns]; //instanciando  matriz 
+	}
+	
+	
+	
+	
+
+}
