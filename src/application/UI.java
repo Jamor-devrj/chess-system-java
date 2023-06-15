5c3cd05f267fe9946f0131c1441c293a5504eba0
jamor <jamor.devbr@gmail.com>
2023-06-15 14:01:46 -0300

    Special move - Castling

--- src/application/UI.java
+++ src/application/UI.java
@@ -77,27 +77,31 @@ public class UI {
 	public static void printBoard(ChessPiece[][] pieces) {
 
 		for (int i = 0; i < pieces.length; i++) {
-			System.out.print((8 - i) + " ");
+			System.out.print(ANSI_WHITE_BACKGROUND + ANSI_BLACK + "  " +(8 - i) + ANSI_RESET + " ");
 
 			for (int j = 0; j < pieces.length; j++) {
 				printPiece(pieces[i][j], false);
 			}
 			System.out.println();
 		}
-		System.out.print("  a b c d e f g h\n");
+		System.out.print(ANSI_WHITE_BACKGROUND + ANSI_BLACK + "    a b c d e f g h\n");
+		System.out.println("                   ");
+		System.out.println(ANSI_RESET);
 	}
 	
 	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
 
 		for (int i = 0; i < pieces.length; i++) {
-			System.out.print((8 - i) + " ");
+			System.out.print(ANSI_WHITE_BACKGROUND + ANSI_BLACK + "  " + (8 - i) + ANSI_RESET + " ");
 
 			for (int j = 0; j < pieces.length; j++) {
 				printPiece(pieces[i][j], possibleMoves[i][j]);
 			}
 			System.out.println();
 		}
-		System.out.print("  a b c d e f g h");
+		System.out.print(ANSI_WHITE_BACKGROUND + ANSI_BLACK + "    a b c d e f g h\n");
+		System.out.println("                   ");
+		System.out.println(ANSI_RESET);
 	}
 
 	
